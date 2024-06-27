32c3b34640ff2eaba737bf52ef95b65231d5aaf0
Keigo Oka <ogiekako@gmail.com>
2024-06-27 18:47:26 +0900

    Add common
    cargo t

--- oka/src/main.rs
+++ oka/src/main.rs
@@ -1,3 +1,19 @@
+use common;
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", hello_world());
+}
+
+fn hello_world() -> String {
+    format!("{}, world!", common::hello())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::hello_world;
+
+    #[test]
+    fn test_hello() {
+        assert_eq!(hello_world(), "Hello, world!");
+    }
 }
