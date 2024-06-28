5c8de0a6fe4adae4ebdf831bfa81a45d35dd3302
Masaya Suzuki <draftcode@gmail.com>
2024-06-28 09:53:05 -0700

    Use the proxied API

--- communicate/src/main.rs
+++ communicate/src/main.rs
@@ -11,10 +11,17 @@ fn get_api_token_from_env() -> String {
     std::env::var("API_TOKEN").unwrap_or_default()
 }
 
+fn get_api_url_from_env() -> String {
+    std::env::var("API_URL").unwrap_or("https://icfp-api.badalloc.com/communicate".to_string())
+}
+
 #[derive(Parser, Debug)]
 struct Args {
     #[arg(long, default_value_t = get_api_token_from_env())]
     api_token: String,
+
+    #[arg(long, default_value_t = get_api_url_from_env())]
+    api_url: String,
 
     request: Option<String>,
 }
@@ -36,7 +43,7 @@ fn main() -> Result<ExitCode> {
         let input_expr = Expr::String(request);
 
         let response = client
-            .post("https://boundvariable.space/communicate")
+            .post(args.api_url)
             .header("Authorization", format!("Bearer {}", args.api_token))
             .header("Content-Type", "text/plain")
             .body(input_expr.to_string())
