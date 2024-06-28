60c4ef5f41d3f04ce2f3b9936dd1c2fc9ee4b068
Masaya Suzuki <draftcode@gmail.com>
2024-06-28 10:57:28 -0700

    Add API for querying the recent communication logs

--- backend_py/src/backend_py/api.py
+++ backend_py/src/backend_py/api.py
@@ -1,11 +1,13 @@
 import httpx
 from backend_rs import decode_message  # type: ignore
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Query
 from fastapi.responses import RedirectResponse, PlainTextResponse
 from .config import settings
 from .deps import SessionDep
 from .models import CommunicationLog
+from sqlmodel import select
 import datetime
+from typing import Sequence
 
 app = FastAPI()
 http_client = httpx.Client(headers={"Authorization": f"Bearer {settings.API_TOKEN}"})
@@ -36,3 +38,24 @@ async def communicate(
     session.commit()
 
     return resp_str
+
+
+@app.get("/communications")
+async def communications(
+    session: SessionDep,
+    decoded_request: str | None = None,
+    decoded_request_prefix: str | None = None,
+    offset: int = 0,
+    limit: int = Query(default=10),
+) -> Sequence[CommunicationLog]:
+    q = select(CommunicationLog)
+    if decoded_request:
+        q = q.where(CommunicationLog.decoded_request == decoded_request)
+    if decoded_request_prefix:
+        q = q.where(CommunicationLog.decoded_request.like(decoded_request_prefix + "%"))  # type: ignore
+
+    return session.exec(
+        q.order_by(CommunicationLog.id.desc())  # type: ignore
+        .offset(offset)
+        .limit(limit)
+    ).all()
