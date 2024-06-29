b0f45436bb282bca33dfd5dd146798b2088d9795
chiro <i@chir.jp>
2024-06-29 14:03:49 +0900

    planer: read input

--- chir/interpreter/src/main.rs
+++ chir/interpreter/src/main.rs
@@ -8,19 +8,20 @@ use common::planar::{Cell, Board, State};
 fn main() -> Result<()> {
     let s = io::read_to_string(io::stdin())?;
 
+    let mut it = s.lines();
+    let first = it.next().unwrap().split_whitespace().collect::<Vec<&str>>();
+
     let mut state: State = Default::default();
-    for l in s.lines() {
+    state.input_a = first[0].parse::<i32>()?;
+    state.input_b = first[1].parse::<i32>()?;
+
+    for l in it {
         let mut row = vec![];
         for c in l.split_whitespace() {
             row.push(Cell::from_str(c)?);
         }
         state.board.0.push(row);
     }
-
-    state.input_a = 5;
-    state.input_b = 5;
-
-    println!("{:?}", state);
 
     println!("{}", state.board);
 
