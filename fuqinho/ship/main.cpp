beba253dfdef4fce0b4a26bcb2df3c0570e57962
Naoki Fukino <naoki.fukino@gmail.com>
2024-06-30 04:54:28 +0900

    Initial solution for spaceship.

---
+++ fuqinho/ship/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+struct Vec2d {
+    int x, y;
+    Vec2d(int x, int y) : x(x), y(y) {}
+};
+
+int main() {
+
+
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
