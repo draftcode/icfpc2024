ad5b13071eb75424915838855122ad0ae925729d
Keigo Oka <ogiekako@gmail.com>
2024-06-30 17:38:28 +0900

    .

--- oka/sandbox/0.scm
+++ oka/sandbox/0.scm
@@ -1,13 +1,19 @@
 (define (string-take n s) (substring s 0 n))
 (define (string-drop n s) (substring s n (string-length s)))
 
-(define (x4 x) (string-append (string-append x x) (string-append x x)))
-(define (res2) (x4 (x4 (x4 "RRRR"))))
+(define (double x) (string-append x x))
 
-(define (res) (
-    string-append
-    "solve lambdaman6 "
-    (res2)
-))
+(define (solve rng iter) (if (= iter 0) "" (
+  string-append
+  (solve 
+    (modulo (* rng 48271) 2147483647) (- iter 1))
+  (double (string-take 1 (string-drop (div rng 536870912) "DLUR")))
+)))
+
+(define (res) 
+  (string-append "solve lambdaman11 " (
+    solve 1 50000
+  ))
+)
 
 (define (main args) (print (res)))
