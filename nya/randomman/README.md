86561fe3debdd47d514379cb8e0d5f7480b8345b
Shuhei Takahashi <takahashi.shuhei@gmail.com>
2024-07-01 02:08:05 +0900

    randomman

--- nya/randomman/README.md
+++ nya/randomman/README.md
@@ -5,19 +5,19 @@
 Searching seeds:
 
 ```
-$ cargo run --release -p randomman -- search [--rng=<name>] [--stride=<N>] <problemID>
+$ cargo run --release -p randomman -- search --stride=<N> [--rng=<name>] [--start-seed=<seed>] <problemID>
 ```
 
 Compiling code with a seed:
 
 ```
-$ cargo run --release -p randomman -- compile [--rng=<name>] [--stride=<N>] <problemID> <seed>
+$ cargo run --release -p randomman -- compile --stride=<N> [--rng=<name>] <problemID> <seed>
 ```
 
 Compiling and submitting code with a seed:
 
 ```
-$ cargo run --release -p randomman -- submit [--rng=<name>] [--stride=<N>] <problemID> <seed>
+$ cargo run --release -p randomman -- submit --stride=<N> [--rng=<name>] <problemID> <seed>
 ```
 
 Submitting all known solutions:
--- nya/randomman/src/main.rs
+++ nya/randomman/src/main.rs
@@ -2,7 +2,7 @@ use std::{
     path::{Path, PathBuf},
     rc::Rc,
     str::FromStr,
-    sync::atomic::{AtomicUsize, Ordering},
+    sync::atomic::{AtomicBool, AtomicUsize, Ordering},
 };
 
 use anyhow::{bail, ensure, Context, Result};
@@ -14,6 +14,8 @@ use crate::assembler::ToExpr;
 
 #[macro_use]
 mod assembler;
+
+const MAX_MOVES: usize = 1_000_000;
 
 #[derive(Debug, Clone, Eq, PartialEq)]
 enum Rng {
@@ -230,7 +232,7 @@ struct Args {
 #[derive(Subcommand, Debug)]
 enum Command {
     Search {
-        #[arg(long, default_value_t = 2)]
+        #[arg(long, required = true)]
         stride: usize,
 
         #[arg(long, default_value = "default")]
@@ -242,8 +244,11 @@ enum Command {
         problem_id: usize,
     },
     Compile {
-        #[arg(long, default_value_t = 2)]
+        #[arg(long, required = true)]
         stride: usize,
+
+        #[arg(long, default_value_t = MAX_MOVES)]
+        moves: usize,
 
         #[arg(long, default_value = "default")]
         rng: String,
@@ -253,8 +258,11 @@ enum Command {
         seed: u64,
     },
     Submit {
-        #[arg(long, default_value_t = 2)]
+        #[arg(long, required = true)]
         stride: usize,
+
+        #[arg(long, default_value_t = MAX_MOVES)]
+        moves: usize,
 
         #[arg(long, default_value = "default")]
         rng: String,
@@ -276,11 +284,13 @@ fn search_main(problem_id: usize, stride: usize, rng_name: &str, start_seed: u64
 
     const CHUNK_SIZE: usize = 1000;
     const SEED_MAX: u64 = 1000000000;
-    let steps = 1000000 / stride;
+    let steps = MAX_MOVES / stride;
 
     for start in (start_seed..SEED_MAX).step_by(CHUNK_SIZE) {
         let end = start + CHUNK_SIZE as u64 - 1;
         eprint!("{}...\r", end);
+
+        let solved = AtomicBool::new(false);
 
         (start..=end).into_par_iter().for_each(|seed| {
             let mut game = game.clone();
@@ -295,6 +305,7 @@ fn search_main(problem_id: usize, stride: usize, rng_name: &str, start_seed: u64
                 if game.pills() == 0 {
                     eprintln!("seed {seed}: all pills eaten in {} steps!", step * stride);
                     best_pills.store(0, Ordering::SeqCst);
+                    solved.store(true, Ordering::SeqCst);
                     return;
                 }
             }
@@ -304,18 +315,28 @@ fn search_main(problem_id: usize, stride: usize, rng_name: &str, start_seed: u64
                 eprintln!("seed {seed}: {pills} pills");
             }
         });
+
+        if solved.load(Ordering::SeqCst) {
+            break;
+        }
     }
 
     Ok(())
 }
 
-fn compile_expr(problem_id: usize, seed: u64, stride: usize, rng: &Rng) -> Result<Expr> {
+fn compile_expr(
+    problem_id: usize,
+    seed: u64,
+    stride: usize,
+    moves: usize,
+    rng: &Rng,
+) -> Result<Expr> {
     let rng_expr = rng.expr();
 
     let header = format!("solve lambdaman{problem_id} ");
     let seed = seed as u128;
 
-    let steps = (1000000 / stride) as u128;
+    let steps = (moves / stride) as u128;
     let step_expr = match stride {
         1 => icfp! { (take 1 (drop (/ s 4611686018427387904) "LUDR")) },
         2 => icfp! { (take 2 (drop (* (/ s 4611686018427387904) 2) "LLUUDDRR")) },
@@ -335,10 +356,17 @@ fn compile_expr(problem_id: usize, seed: u64, stride: usize, rng: &Rng) -> Resul
     Ok(expr)
 }
 
-fn compile_main(problem_id: usize, seed: u64, stride: usize, rng_name: &str) -> Result<()> {
+fn compile_main(
+    problem_id: usize,
+    seed: u64,
+    stride: usize,
+    moves: usize,
+    rng_name: &str,
+) -> Result<()> {
     let rng = Rng::from_name(rng_name).context("unknown RNG name")?;
-    let expr = compile_expr(problem_id, seed, stride, &rng)?;
+    let expr = compile_expr(problem_id, seed, stride, moves, &rng)?;
     println!("{}", expr.encoded());
+    eprintln!("({} bytes)", expr.encoded().to_string().len());
     Ok(())
 }
 
@@ -373,9 +401,15 @@ fn do_submit(problem_id: usize, expr: &Expr) -> Result<()> {
     Ok(())
 }
 
-fn submit_main(problem_id: usize, seed: u64, stride: usize, rng_name: &str) -> Result<()> {
+fn submit_main(
+    problem_id: usize,
+    seed: u64,
+    stride: usize,
+    moves: usize,
+    rng_name: &str,
+) -> Result<()> {
     let rng = Rng::from_name(rng_name).context("unknown RNG name")?;
-    let expr = compile_expr(problem_id, seed, stride, &rng)?;
+    let expr = compile_expr(problem_id, seed, stride, moves, &rng)?;
     do_submit(problem_id, &expr)?;
     Ok(())
 }
@@ -385,51 +419,100 @@ struct KnownSolution {
     rng: Rng,
     seed: u64,
     stride: usize,
+    moves: usize,
 }
 
 // ***HELP ME***: Please add known solutions here.
 const KNOWN_SOLUTIONS: &[KnownSolution] = &[
+    // 1-3 have shorter solutions
+    KnownSolution {
+        problem_id: 4,
+        rng: Rng::Default,
+        seed: 29,
+        stride: 1,
+        moves: 17042,
+    },
+    // TODO: 5 can be revisited if code gets shorter
+    // 6 has a shorter solution
+    KnownSolution {
+        problem_id: 7,
+        rng: Rng::Default,
+        seed: 298,
+        stride: 1,
+        moves: 17860,
+    },
+    // 8-9 have shorter solutions
+    KnownSolution {
+        problem_id: 10,
+        rng: Rng::Default,
+        seed: 1,
+        stride: 1,
+        moves: 53394,
+    },
     KnownSolution {
         problem_id: 11,
         rng: Rng::Default,
         seed: 4610551,
         stride: 2,
+        moves: MAX_MOVES,
     },
     KnownSolution {
         problem_id: 12,
         rng: Rng::Default,
         seed: 663880,
         stride: 2,
+        moves: MAX_MOVES,
     },
     KnownSolution {
         problem_id: 13,
         rng: Rng::Default,
         seed: 217404,
         stride: 2,
+        moves: MAX_MOVES,
     },
     KnownSolution {
         problem_id: 14,
         rng: Rng::Default,
         seed: 35975,
         stride: 2,
+        moves: MAX_MOVES,
     },
     KnownSolution {
         problem_id: 15,
         rng: Rng::Default,
         seed: 1663183,
         stride: 2,
+        moves: MAX_MOVES,
+    },
+    // 16 is hard
+    KnownSolution {
+        problem_id: 17,
+        rng: Rng::Default,
+        seed: 9,
+        stride: 1,
+        moves: MAX_MOVES,
     },
     KnownSolution {
         problem_id: 18,
         rng: Rng::Default,
         seed: 288180,
         stride: 1,
+        moves: MAX_MOVES,
     },
+    // 19 is hard
+    // 20 is hard
+    // TODO: 21 looks solvable
 ];
 
 fn submit_all_main() -> Result<()> {
     for known in KNOWN_SOLUTIONS {
-        let expr = compile_expr(known.problem_id, known.seed, known.stride, &known.rng)?;
+        let expr = compile_expr(
+            known.problem_id,
+            known.seed,
+            known.stride,
+            known.moves,
+            &known.rng,
+        )?;
         do_submit(known.problem_id, &expr)?;
     }
     Ok(())
@@ -448,15 +531,17 @@ fn main() -> Result<()> {
         Command::Compile {
             rng,
             stride,
+            moves,
             problem_id,
             seed,
-        } => compile_main(problem_id, seed, stride, &rng),
+        } => compile_main(problem_id, seed, stride, moves, &rng),
         Command::Submit {
             rng,
             stride,
+            moves,
             problem_id,
             seed,
-        } => submit_main(problem_id, seed, stride, &rng),
+        } => submit_main(problem_id, seed, stride, moves, &rng),
         Command::SubmitAll => submit_all_main(),
     }
 }
