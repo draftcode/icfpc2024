309201e9c78d8f8b790e18d870b195dec2d29862
Keigo Oka <ogiekako@gmail.com>
2024-07-01 18:03:24 +0900

    328

--- oka/19.scm
+++ oka/19.scm
@@ -1,13 +1,13 @@
 (define (string-take n s) (substring s 0 n))
 (define (string-drop n s) (substring s n (string-length s)))
 
-(define (rep i d) (if (= d 1) (string-take 1 (string-drop i "URDLUR")) (string-append (rep i (- d 1)) (rep i (- d 1)))))
+(define (rep i d) (if (= d 1) (string-take 1 (string-drop i "URDLURD")) (string-append (rep i (- d 1)) (rep i (- d 1)))))
 
 (define (recur d p) (
-    if (or (= d 0) (< p 0)) ""
+    if (< (* d p) 1) ""
     (string-append
         (rep p d)
-        (recur (- d 1) 3)
+        (recur (- d 1) 4)
         (rep (+ p 2) d)
         (recur d (- p 1))
     )
@@ -16,7 +16,7 @@
 (define (res) (
     string-append
     "solve lambdaman19 "
-    (recur 7 3)
+    (recur 7 4)
 ))
 
 (define (main args) (print (res)))
