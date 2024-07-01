4ff192c10c45135aaa005aedabf8f6e4b737829c
Keigo Oka <ogiekako@gmail.com>
2024-07-01 17:57:06 +0900

    333

--- oka/19.scm
+++ oka/19.scm
@@ -1,16 +1,14 @@
 (define (string-take n s) (substring s 0 n))
 (define (string-drop n s) (substring s n (string-length s)))
 
-(define (rep s d) (if (= d 1) s (string-append (rep s (- d 1)) (rep s (- d 1)))))
-
-(define (get p) (string-take 1 (string-drop p "URDLUR")))
+(define (rep i d) (if (= d 1) (string-take 1 (string-drop i "URDLUR")) (string-append (rep i (- d 1)) (rep i (- d 1)))))
 
 (define (recur d p) (
     if (or (= d 0) (< p 0)) ""
     (string-append
-        (rep (get p) d)
+        (rep p d)
         (recur (- d 1) 3)
-        (rep (get (+ p 2)) d)
+        (rep (+ p 2) d)
         (recur d (- p 1))
     )
 ))
