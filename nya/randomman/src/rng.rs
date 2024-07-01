fa7ce9a822727505ab2f42ed6c6d724716c405ab
Keigo Oka <ogiekako@gmail.com>
2024-07-01 11:30:36 +0900

    fix

--- nya/randomman/src/rng.rs
+++ nya/randomman/src/rng.rs
@@ -117,6 +117,7 @@ impl Rng {
             Self::SmallModRev => 207645,
             Self::MediumModRev => 536870912,
             Self::Better | Self::Default | Self::DefaultRev => 4611686018427387904,
+            Self::Reference => unreachable!(),
         };
 
         let step_expr = match stride {
