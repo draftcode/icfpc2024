e29854fb047f4db0e13a2c6f9b8bc8eaec6f4b44
Keigo Oka <ogiekako@gmail.com>
2024-07-01 15:43:58 +0900

    LM9: 110B

--- oka/9.scm
+++ oka/9.scm
@@ -2,12 +2,12 @@
 (define (string-drop n s) (substring s n (string-length s)))
 
 (define (d1 x) (string-append x x x))
-(define (d2 x) (d1 (d1 (d1 x))))
+(define (d2 x) (string-append (d1 (d1 (d1 x))) "D"))
 
 (define (res) (
     string-append
     "solve lambdaman9 "
-    (d2 (d2 (string-append (d2 "RR") "D" (d2 "LL"))))
+    (d2 (string-append (d2 "RR") (d2 "LL")))
 ))
 
 (define (main args) (print (res)))
